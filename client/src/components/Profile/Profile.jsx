6c92b3ebb91ccb30d8e716281e7a74ed1aa40d8d
Victor Raul <v1c70rotakmer@gmail.com>
2023-10-17 17:10:09 -0500

    Fix: corregir nombres de variables

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -6,34 +6,34 @@ import { useNavigate } from "react-router-dom"
 import ProfileDectail from "./ProfileDectail"
 
 const Profile = () =>{
-  const [chagePicture , setChagePicture] = useState(false)
+  const [changePicture , setChangePicture] = useState(false)
   const [section , setSection] = useState('')
   const { auth ,logOut}= useAuth()
   const navigate = useNavigate()
-  const showSecction = (area)=>{
+  const showSection = (area)=>{
     setSection(area)
   }
   const callLogOut = ()=>{
     console.log('esto cerrara la seccion');
     
-     logOut();
+      logOut();
       navigate('/');
   }
 
   return (
     <div className="flex flex-col h-[95%]">
     <section className=" pt-0">
-        <div className="menu-logo">
-        <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12H20M4 12L8 8M4 12L8 16" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-        <p className="userInfo-subTitle">Settings</p>  
-        <div > 
-            <svg className="billContainer-svg" width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9.71069 18.2929C10.1012 18.6834 10.7344 18.6834 11.1249 18.2929L16.0123 13.4006C16.7927 12.6195 16.7924 11.3537 16.0117 10.5729L11.1213 5.68254C10.7308 5.29202 10.0976 5.29202 9.70708 5.68254C9.31655 6.07307 9.31655 6.70623 9.70708 7.09676L13.8927 11.2824C14.2833 11.6729 14.2833 12.3061 13.8927 12.6966L9.71069 16.8787C9.32016 17.2692 9.32016 17.9023 9.71069 18.2929Z" fill="#808080"></path> </g></svg> 
-        </div>
-        <p className="userInfo-name">Profile</p>
-        </div>
-        <header className="billContainer-header m-0 mt-3" >
-        <p className="billContainer-title text-5xl py-3">Profile</p>
-        </header>
+      <div className="menu-logo">
+      <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12H20M4 12L8 8M4 12L8 16" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+      <p className="userInfo-subTitle">Settings</p>  
+      <div > 
+        <svg className="billContainer-svg" width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9.71069 18.2929C10.1012 18.6834 10.7344 18.6834 11.1249 18.2929L16.0123 13.4006C16.7927 12.6195 16.7924 11.3537 16.0117 10.5729L11.1213 5.68254C10.7308 5.29202 10.0976 5.29202 9.70708 5.68254C9.31655 6.07307 9.31655 6.70623 9.70708 7.09676L13.8927 11.2824C14.2833 11.6729 14.2833 12.3061 13.8927 12.6966L9.71069 16.8787C9.32016 17.2692 9.32016 17.9023 9.71069 18.2929Z" fill="#808080"></path> </g></svg> 
+      </div>
+      <p className="userInfo-name">Profile</p>
+      </div>
+      <header className="billContainer-header m-0 mt-3" >
+      <p className="billContainer-title text-5xl py-3">Profile</p>
+      </header>
     </section>
     <section className="profileContainer flex-1">
       <div  className="profileCard h-full justify-between">
@@ -41,7 +41,7 @@ const Profile = () =>{
           <div className="profileImgContainer h-full">
             <img className="profileImg" src={auth.image} alt="" />
             
-            <div onClick={()=>setChagePicture(!chagePicture)} className="profileImgEdit" >
+            <div onClick={()=>setChangePicture(!changePicture)} className="profileImgEdit" >
               <img src={pen} alt="" />
             </div>
           </div>
@@ -61,12 +61,12 @@ const Profile = () =>{
             
           </main>
           <div>
-            <div className='profile-settings ' onClick={()=>showSecction('Personal')}>
+            <div className='profile-settings ' onClick={()=>showSection('Personal')}>
               <img className="profileImg sm" src={user} alt="ssf" />
             <p className={section =='Personal'?"profile-title":"profile-title-lg"} >Personal Information</p>
             {section =='Personal' && <div className='profile-bar ' ></div>}
             </div>
-            <div className='profile-settings ' onClick={()=>showSecction('Login')} >
+            <div className='profile-settings ' onClick={()=>showSection('Login')} >
               <img className="profileImg sm" src={lock} alt="ssf" />
             <p className={section =='Login'?"profile-title":"profile-title-lg"} >Login & Password</p>
           {  section =='Login' &&   <div className='profile-bar ' ></div>}
@@ -87,7 +87,7 @@ const Profile = () =>{
       </div>
 
       <div>
-        {section =='Login' && <ProfileDectail chagePicture={chagePicture}  setChagePicture={setChagePicture}/>}
+        {section =='Login' && <ProfileDectail changePicture={changePicture}  setChangePicture={setChangePicture}/>}
       </div>
     </section>
     </div>
